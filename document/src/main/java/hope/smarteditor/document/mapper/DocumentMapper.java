3469c6a6638c85afb0294673d273d11f49e9b7f2
lzh <2212384795@qq.com>
2024-05-23 19:06:55 +0800

    feat(): DocumentMapper

---
+++ document/src/main/java/hope/smarteditor/document/mapper/DocumentMapper.java
@@ -0,0 +1,18 @@
+package hope.smarteditor.document.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import hope.smarteditor.common.model.entity.Document;
+
+/**
+* @author LoveF
+* @description 针对表【document】的数据库操作Mapper
+* @createDate 2024-05-22 14:26:07
+* @Entity generator.domain.Document
+*/
+public interface DocumentMapper extends BaseMapper<Document> {
+
+}
+
+
+
+
