f67b9ddcad0217cad38f134a59917da97e5c4522
lzh <2212384795@qq.com>
2024-05-27 19:00:12 +0800

    fix(): document模块启动类

--- document/src/main/java/hope/smarteditor/document/DocumentApplication.java
+++ document/src/main/java/hope/smarteditor/document/DocumentApplication.java
@@ -1,13 +1,21 @@
 package hope.smarteditor.document;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
+@MapperScan("hope.smarteditor.document.mapper")
+@EnableCaching//开发缓存注解功能
+@EnableDubbo
 public class DocumentApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DocumentApplication.class, args);
+
     }
 
 }
