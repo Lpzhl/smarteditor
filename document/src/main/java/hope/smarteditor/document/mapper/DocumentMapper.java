ab8fc970b136808f250e6ab38cc2f0525d93e7f2
lzh <2212384795@qq.com>
2024-05-27 18:58:05 +0800

    feat(): document模块的mapper层

--- document/src/main/java/hope/smarteditor/document/mapper/DocumentMapper.java
+++ document/src/main/java/hope/smarteditor/document/mapper/DocumentMapper.java
@@ -2,6 +2,7 @@ package hope.smarteditor.document.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import hope.smarteditor.common.model.entity.Document;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
 * @author LoveF
@@ -9,6 +10,7 @@ import hope.smarteditor.common.model.entity.Document;
 * @createDate 2024-05-22 14:26:07
 * @Entity generator.domain.Document
 */
+@Mapper
 public interface DocumentMapper extends BaseMapper<Document> {
 
 }
