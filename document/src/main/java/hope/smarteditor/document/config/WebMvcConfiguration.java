a1af3731833846758c6f88ee185cc1283a270477
lzh <2212384795@qq.com>
2024-05-27 14:35:09 +0800

    feat(): user模块的config配置类

---
+++ document/src/main/java/hope/smarteditor/document/config/WebMvcConfiguration.java
@@ -0,0 +1,59 @@
+package hope.smarteditor.document.config;
+
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 配置类，注册web层相关组件
+ */
+@Configuration
+@Slf4j
+@EnableKnife4j
+@EnableSwagger2
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+
+    @Bean
+    public Docket docket1(){
+        log.info("准备生成接口文档...");
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("智能编辑器接口文档")
+                .version("2.0")
+                .description("智能编辑器项目接口文档")
+                .build();
+
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("用户端接口")
+                .apiInfo(apiInfo)
+                .select()
+                //指定生成接口需要扫描的包
+                .apis(RequestHandlerSelectors.basePackage("hope.smarteditor.document.controller"))
+                .paths(PathSelectors.any())
+                .build();
+
+        return docket;
+    }
+
+
+    /**
+     * 设置静态资源映射，主要是访问接口文档（html、js、css）
+     * @param registry
+     */
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始设置静态资源映射...");
+        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
