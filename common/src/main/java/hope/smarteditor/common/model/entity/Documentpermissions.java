ea25d1ec30927ddbed27af2087b7b00eb8e79288
lzh <2212384795@qq.com>
2024-05-28 14:57:48 +0800

    feat(): 文档权限实体类

---
+++ common/src/main/java/hope/smarteditor/common/model/entity/Documentpermissions.java
@@ -0,0 +1,25 @@
+package hope.smarteditor.common.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * @TableName documentpermissions
+ */
+@TableName(value ="documentpermissions")
+@Data
+public class Documentpermissions implements Serializable {
+    private Long id;
+
+    private Long documentId;
+
+    private Long userId;
+
+    private Long permissionId;
+
+    private static final long serialVersionUID = 1L;
+}
