65a7f715c29ecdc04737941eba6ccc5994d5df1e
lzh <2212384795@qq.com>
2024-05-31 17:05:23 +0800

    feat(): 权限mapper

---
+++ document/src/main/java/hope/smarteditor/document/mapper/PermissionsMapper.java
@@ -0,0 +1,18 @@
+package hope.smarteditor.document.mapper;
+
+import hope.smarteditor.common.model.entity.Permissions;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author LoveF
+* @description 针对表【permissions】的数据库操作Mapper
+* @createDate 2024-05-28 20:33:38
+* @Entity hope.smarteditor.common.model.entity.Permissions
+*/
+public interface PermissionsMapper extends BaseMapper<Permissions> {
+
+}
+
+
+
+
