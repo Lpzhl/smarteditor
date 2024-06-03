3574896fa87602397fc4b3889a4711667fdc81bd
lzh <2212384795@qq.com>
2024-06-03 21:37:47 +0800

    fix(): 增加了redis缓存文档

--- document/src/main/java/hope/smarteditor/document/service/impl/DocumentServiceImpl.java
+++ document/src/main/java/hope/smarteditor/document/service/impl/DocumentServiceImpl.java
@@ -18,6 +18,7 @@ import io.minio.errors.MinioException;
 import io.minio.http.Method;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,6 +28,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.Date;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
 * @author LoveF
@@ -46,6 +48,9 @@ public class DocumentServiceImpl extends ServiceImpl<DocumentMapper, Document>
     @Resource
     private DocumentpermissionsMapper documentpermissionsMapper;
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     @Value("${minio.bucket-name}")
     private String bucketName;
 
@@ -61,7 +66,6 @@ public class DocumentServiceImpl extends ServiceImpl<DocumentMapper, Document>
             if (!isExist) {
                 minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
                 //设置存储桶的访问权限
-
             }
             minioClient.putObject(
                     PutObjectArgs.builder()
@@ -71,7 +75,6 @@ public class DocumentServiceImpl extends ServiceImpl<DocumentMapper, Document>
                             .contentType(file.getContentType())
                             .build()
             );
-
             String fullUrl = minioClient.getPresignedObjectUrl(
                     GetPresignedObjectUrlArgs.builder()
                             .method(Method.GET)
@@ -80,7 +83,6 @@ public class DocumentServiceImpl extends ServiceImpl<DocumentMapper, Document>
                             //.expiry(60 * 60 * 24) // URL有效期为24小时
                             .build()
             );
-
             URL url = new URL(fullUrl);
             String baseUrl = url.getProtocol() + "://" + url.getHost() +":9000"+ url.getPath();
 
@@ -92,6 +94,11 @@ public class DocumentServiceImpl extends ServiceImpl<DocumentMapper, Document>
     }
 
 
+    /**
+     * 保存文件信息
+     * @param documentUploadDTO
+     * @return
+     */
     @Override
     public Document saveDocument(DocumentUploadDTO documentUploadDTO) {
         Document document = new Document();
@@ -107,28 +114,36 @@ public class DocumentServiceImpl extends ServiceImpl<DocumentMapper, Document>
 
         int insert = documentMapper.insert(document);
 
-        if(insert == 0){
+        if (insert == 0) {
             throw new BusinessException(ErrorCode.SAVE_FILE_ERROR);
         }
 
         Document savedDocument = documentMapper.selectById(document.getId());
 
-        //设置文档权限
+        // 设置文档权限
         Documentpermissions documentpermissions = new Documentpermissions();
         documentpermissions.setDocumentId(document.getId());
         documentpermissions.setUserId(document.getUserId());
-        documentpermissions.setPermissionId(1L); //设置为创建者 权限为可编辑
+        documentpermissions.setPermissionId(1L); // 设置为创建者，权限为可编辑
         documentpermissionsMapper.insert(documentpermissions);
+
+        // 保存到Redis中，以文档的ID作为key
+        String cacheKey = "document:" + document.getId();
+        redisTemplate.opsForValue().set(cacheKey, savedDocument);
+        // 设置缓存过期时间，例如1小时
+        redisTemplate.expire(cacheKey, 7, TimeUnit.DAYS);
+
 
         return savedDocument;
     }
 
-    @Override
-    public boolean deleteDocument(Long documentId) {
-        int i = documentMapper.deleteById(documentId);
-        return i > 0;
-    }
 
+    /**
+     * 更新文件信息
+     * @param documentId
+     * @param documentUpdateDTO
+     * @return
+     */
     @Override
     public Document updateDocument(Long documentId, DocumentUpdateDTO documentUpdateDTO) {
         Document document =documentMapper.selectById(documentId);
@@ -159,30 +174,68 @@ public class DocumentServiceImpl extends ServiceImpl<DocumentMapper, Document>
         document.setUpdateTime(new Date());
         documentMapper.updateById(document);
 
+        // 更新Redis中缓存的信息
+        String cacheKey = "document:" + documentId;
+        Boolean hasKey = redisTemplate.hasKey(cacheKey);
+        if (hasKey != null && hasKey) {
+            Document updatedDocument = documentMapper.selectById(documentId);
+            redisTemplate.opsForValue().set(cacheKey, updatedDocument);
+            redisTemplate.expire(cacheKey, 7, TimeUnit.DAYS);
+        } else {
+            redisTemplate.opsForValue().set(cacheKey, document);
+        }
+
+
         return document;
     }
 
+
+    @Override
+    public boolean deleteDocument(Long documentId) {
+        int i = documentMapper.deleteById(documentId);
+        // 删除缓存的信息
+        if (i > 0) {
+            String cacheKey = "document:" + documentId;
+            redisTemplate.delete(cacheKey);
+        }
+        return i > 0;
+    }
+
+
     @Override
     public void setDocumentVisibility(Long documentId) {
-        //1. 查询文档对象
+        // 1. 查询文档对象
         QueryWrapper<Document> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("id", documentId);
         Document document = documentMapper.selectOne(queryWrapper);
 
-        //2. 获取当前文档的可见性状态
+        // 2. 获取当前文档的可见性状态
         Integer visibility = document.getVisibility();
 
-        //3. 根据当前可见性状态，进行设置
-        if(visibility == 1) {
-            //如果当前文档是公开的，则将其设置为私有
+        // 3. 根据当前可见性状态，进行设置
+        if (visibility == 1) {
+            // 如果当前文档是公开的，则将其设置为私有
             document.setVisibility(0);
         } else if (visibility == 0) {
-            //如果当前文档是私有的，则将其设置为公开
+            // 如果当前文档是私有的，则将其设置为公开
             document.setVisibility(1);
         }
-        //4. 更新文档对象
+
+        // 4. 更新文档对象
         documentMapper.updateById(document);
+
+        // 5. 同步更新缓存信息
+        String cacheKey = "document:" + documentId;
+        Boolean hasKey = redisTemplate.hasKey(cacheKey);
+        if (hasKey != null && hasKey) {
+            // 更新缓存中的文档信息
+            Document updatedDocument = documentMapper.selectById(documentId);
+            redisTemplate.opsForValue().set(cacheKey, updatedDocument);
+            // 设置缓存过期时间，例如7天
+            redisTemplate.expire(cacheKey, 7, TimeUnit.DAYS);
+        }
     }
+
 
 
 
