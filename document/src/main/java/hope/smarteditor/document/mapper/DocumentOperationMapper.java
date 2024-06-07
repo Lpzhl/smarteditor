a86557b461a4cb56080d973789c4a353615c174a
lzh <2212384795@qq.com>
2024-06-07 16:40:19 +0800

    feat(): 文档版本控Mapper

---
+++ document/src/main/java/hope/smarteditor/document/mapper/DocumentOperationMapper.java
@@ -0,0 +1,21 @@
+package hope.smarteditor.document.mapper;
+
+import hope.smarteditor.common.model.entity.DocumentOperation;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author LoveF
+* @description 针对表【document_operation】的数据库操作Mapper
+* @createDate 2024-06-06 17:38:39
+* @Entity hope.smarteditor.common.model.entity.DocumentOperation
+*/
+
+@Mapper
+public interface DocumentOperationMapper extends BaseMapper<DocumentOperation> {
+
+}
+
+
+
+
---
+++ document/src/main/java/hope/smarteditor/document/mapper/DocumentVersionMapper.java
@@ -0,0 +1,21 @@
+package hope.smarteditor.document.mapper;
+
+import hope.smarteditor.common.model.entity.DocumentVersion;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author LoveF
+* @description 针对表【document_version】的数据库操作Mapper
+* @createDate 2024-06-06 17:41:49
+* @Entity hope.smarteditor.common.model.entity.DocumentVersion
+*/
+@Mapper
+public interface DocumentVersionMapper extends BaseMapper<DocumentVersion> {
+
+
+}
+
+
+
+
