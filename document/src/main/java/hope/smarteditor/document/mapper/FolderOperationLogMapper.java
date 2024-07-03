fbb6228e00fdcea780c9368b937da4179873840b
lzh <2212384795@qq.com>
2024-07-03 16:56:11 +0800

    feat(): FolderOperationLogMapper

---
+++ document/src/main/java/hope/smarteditor/document/mapper/FolderOperationLogMapper.java
@@ -0,0 +1,20 @@
+package hope.smarteditor.document.mapper;
+
+import hope.smarteditor.common.model.entity.FolderOperationLog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author LoveF
+* @description 针对表【folder_operation_log】的数据库操作Mapper
+* @createDate 2024-06-09 21:01:37
+* @Entity hope.smarteditor.common.model.entity.FolderOperationLog
+*/
+@Mapper
+public interface FolderOperationLogMapper extends BaseMapper<FolderOperationLog> {
+
+}
+
+
+
+
