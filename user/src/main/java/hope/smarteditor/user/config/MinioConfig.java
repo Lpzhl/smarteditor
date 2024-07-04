a4062534e3668756f87c80b86030fbc14351bf43
lzh <2212384795@qq.com>
2024-07-04 15:18:55 +0800

    feat(): minioConfig

---
+++ user/src/main/java/hope/smarteditor/user/config/MinioConfig.java
@@ -0,0 +1,29 @@
+package hope.smarteditor.user.config;
+
+import io.minio.MinioClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class MinioConfig {
+    @Value("${minio.endpoint}")
+    private String minioEndpoint;
+
+    @Value("${minio.accessKey}")
+    private String minioAccessKey;
+
+    @Value("${minio.secretKey}")
+    private String minioSecretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+       log.info("MinioClient 正在加载");
+        return MinioClient.builder()
+                .endpoint(minioEndpoint)
+                .credentials(minioAccessKey, minioSecretKey)
+                .build();
+    }
+}
