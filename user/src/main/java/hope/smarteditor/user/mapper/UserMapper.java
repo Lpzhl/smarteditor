05dcb4ace52c3f8e9c0f617ac4dff6322ade07c4
lzh <2212384795@qq.com>
2024-07-04 15:19:24 +0800

    feat(): userMapper

---
+++ user/src/main/java/hope/smarteditor/user/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package hope.smarteditor.user.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import hope.smarteditor.common.model.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+
+/**
+* @author LoveF
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2024-05-13 20:51:50
+* @Entity generator.domain.User
+*/
+@Mapper()
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
