dac960278fa10f4fb75e7c185267408716a1da33
lzh <2212384795@qq.com>
2024-07-09 17:27:03 +0800

    feat(): 签到实体类

---
+++ common/src/main/java/hope/smarteditor/common/model/entity/SignIn.java
@@ -0,0 +1,36 @@
+package hope.smarteditor.common.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ *
+ * @TableName sign_in
+ */
+@TableName(value ="sign_in")
+@Data
+public class SignIn implements Serializable {
+    /**
+     *
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     *
+     */
+    private Long userId;
+
+    /**
+     *
+     */
+    private Date signInDate;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
