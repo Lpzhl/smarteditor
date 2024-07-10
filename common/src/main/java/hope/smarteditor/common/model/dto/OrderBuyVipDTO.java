e1612c50a4f822c096a678ce727a691bc08cafa4
lzh <2212384795@qq.com>
2024-07-10 22:16:16 +0800

    feat(): 购买积分和会员DTO

---
+++ common/src/main/java/hope/smarteditor/common/model/dto/OrderBuyVipDTO.java
@@ -0,0 +1,42 @@
+package hope.smarteditor.common.model.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class OrderBuyVipDTO implements Serializable {
+    /**
+     * 用户ID
+     */
+    private Long userId;
+
+    /**
+     * 订单类型 (buy_points: 购买积分, buy_membership: 购买会员)
+     */
+    private String orderType;
+
+    /**
+     * 订单金额
+     */
+    private Integer amount;
+
+    /**
+     * 订单时间
+     */
+    private Date orderTime;
+
+
+    /**
+     * 订单状态 (待支付, 已支付, 已完成等)
+     */
+    private String status;
+
+    /**
+     * 订单描述
+     */
+    private String description;
+
+    private Integer num;
+}
