a1f8eca1ab113dad7ff9b3032d9732b64b68a475
lzh <2212384795@qq.com>
2024-07-15 13:43:23 +0800

    feat(): config

---
+++ document/src/main/java/hope/smarteditor/document/config/AppConfig.java
@@ -0,0 +1,14 @@
+package hope.smarteditor.document.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
