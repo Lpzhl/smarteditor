758264cdd11f7284f1c5714da38912ce761f415f
lzh <2212384795@qq.com>
2024-07-15 13:47:21 +0800

    feat():maven坐标

--- common/src/main/java/hope/smarteditor/common/exception/GlobalExceptionHandler.java
+++ common/src/main/java/hope/smarteditor/common/exception/GlobalExceptionHandler.java
@@ -3,9 +3,14 @@ package hope.smarteditor.common.exception;
 
 import hope.smarteditor.common.constant.ErrorCode;
 import hope.smarteditor.common.result.Result;
+import io.minio.errors.MinioException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 
 /**
@@ -19,13 +24,37 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public Result businessExceptionHandler(BusinessException e) {
-        log.error("businessException: " + e.getMessage(), e);
+        log.error("业务异常: " + e.getMessage(), e);
         return Result.error(e.getMessage());
     }
 
     @ExceptionHandler(RuntimeException.class)
     public Result runtimeExceptionHandler(RuntimeException e) {
-        log.info("测试");
+        log.error("运行时异常: " + e.getMessage(), e);
+        return Result.error(e.getMessage());
+    }
+
+    @ExceptionHandler(MinioException.class)
+    public Result minioExceptionHandler(MinioException e) {
+        log.error("Minio异常: " + e.getMessage(), e);
+        return Result.error(e.getMessage());
+    }
+
+    @ExceptionHandler(IOException.class)
+    public Result ioExceptionHandler(IOException e) {
+        log.error("IO异常: " + e.getMessage(), e);
+        return Result.error(e.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchAlgorithmException.class)
+    public Result noSuchAlgorithmExceptionHandler(NoSuchAlgorithmException e) {
+        log.error("NoSuchAlgorithmException: " + e.getMessage(), e);
+        return Result.error(e.getMessage());
+    }
+
+    @ExceptionHandler(InvalidKeyException.class)
+    public Result invalidKeyExceptionHandler(InvalidKeyException e) {
+        log.error("无效密钥异常: " + e.getMessage(), e);
         return Result.error(e.getMessage());
     }
 }
