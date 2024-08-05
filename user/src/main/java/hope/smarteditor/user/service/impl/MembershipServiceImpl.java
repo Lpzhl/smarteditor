8c92cce234c3c1afdf5eccd5fecdbb3968c4854c
lzh <2212384795@qq.com>
2024-08-05 15:44:25 +0800

    feat: 新增定时器
    每天凌晨12点检测用户会员是否过期

--- user/src/main/java/hope/smarteditor/user/service/impl/MembershipServiceImpl.java
+++ user/src/main/java/hope/smarteditor/user/service/impl/MembershipServiceImpl.java
@@ -2,9 +2,16 @@ package hope.smarteditor.user.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import hope.smarteditor.common.model.entity.Membership;
+import hope.smarteditor.common.model.entity.User;
 import hope.smarteditor.user.service.MembershipService;
 import hope.smarteditor.user.mapper.MembershipMapper;
+import hope.smarteditor.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 /**
 * @author LoveF
@@ -14,6 +21,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class MembershipServiceImpl extends ServiceImpl<MembershipMapper, Membership>
     implements MembershipService{
+
+    @Autowired
+    private MembershipMapper membershipMapper;
+
+    @Autowired
+    private UserService userService;
+
+
+    /**
+     * 定时任务 每天凌晨0点执行一次 检测用户的会员是否到期
+     */
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void checkMembershipExpiry() {
+        List<Membership> expiredMemberships = membershipMapper.findExpiredMemberships();
+
+        for (Membership membership : expiredMemberships) {
+            User user = userService.getById(membership.getUserId());
+            if (user != null) {
+                user.setLevel(0);
+                userService.updateById(user);
+            }
+        }
+    }
 
 }
 
