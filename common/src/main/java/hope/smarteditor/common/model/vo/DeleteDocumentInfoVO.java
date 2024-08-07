4079b11d8b81159d74676de96563e7e7d1b3ea2e
lzh <2212384795@qq.com>
2024-08-07 12:18:01 +0800

    feat: 删除文档信息实体类

---
+++ common/src/main/java/hope/smarteditor/common/model/vo/DeleteDocumentInfoVO.java
@@ -0,0 +1,52 @@
+package hope.smarteditor.common.model.vo;
+
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class DeleteDocumentInfoVO implements Serializable {
+    private Long id;
+
+    private Long userId;
+
+    private String name;
+
+    private String content;
+
+    private String summary;
+
+    private Integer type;
+
+    private String label;
+
+    private String subject;
+
+    private String category;
+
+    private Integer status;
+
+    @TableLogic
+    private Integer isDeleted;
+
+    private Integer likeCount;
+
+    private Integer visibility;
+
+    private String createUserNickname; //创建者的昵称
+
+    private Boolean isFavorite; //是否收藏
+
+    private String originalFolder; // 所在文件夹
+
+    private int day; // 距离彻底删除的天数
+
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date updateTime;
+}
