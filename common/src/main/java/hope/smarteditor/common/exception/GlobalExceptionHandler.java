0eabe72492d46b66728976c51a788eb51c1a153f
lzh <2212384795@qq.com>
2024-08-08 23:34:53 +0800

    feat: 补充sql异常

--- common/src/main/java/hope/smarteditor/common/exception/GlobalExceptionHandler.java
+++ common/src/main/java/hope/smarteditor/common/exception/GlobalExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.sql.SQLSyntaxErrorException;
 
 
 /**
@@ -26,13 +27,25 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     public Result businessExceptionHandler(BusinessException e) {
         log.error("业务异常: " + e.getMessage(), e);
-        return Result.error(e.getMessage());
+        return Result.error("业务异常");
+    }
+
+    @ExceptionHandler(SQLSyntaxErrorException.class)
+    public Result sqlSyntaxErrorExceptionHandler(SQLSyntaxErrorException e) {
+        log.error("SQL 语法错误: " + e.getMessage(), e);
+        return Result.error("数据库查询语法错误 " );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public Result generalExceptionHandler(Exception e) {
+        log.error("运行时异常: " + e.getMessage(), e);
+        return Result.error("系统异常，请联系管理员。");
     }
 
     @ExceptionHandler(RuntimeException.class)
     public Result runtimeExceptionHandler(RuntimeException e) {
         log.error("运行时异常: " + e.getMessage(), e);
-        return Result.error(e.getMessage());
+        return Result.error("系统异常，请联系管理员。");
     }
     @ExceptionHandler(NullPointerException.class)
     public Result nullPointerExceptionHandler(NullPointerException e) {
@@ -43,27 +56,26 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MinioException.class)
     public Result minioExceptionHandler(MinioException e) {
         log.error("Minio异常: " + e.getMessage(), e);
-        return Result.error(e.getMessage());
+        return Result.error("Minio异常");
     }
 
     @ExceptionHandler(IOException.class)
     public Result ioExceptionHandler(IOException e) {
         log.error("IO异常: " + e.getMessage(), e);
-        return Result.error(e.getMessage());
+        return Result.error("系统错误，请联系管理员");
     }
 
     @ExceptionHandler(NoSuchAlgorithmException.class)
     public Result noSuchAlgorithmExceptionHandler(NoSuchAlgorithmException e) {
         log.error("NoSuchAlgorithmException: " + e.getMessage(), e);
-        return Result.error(e.getMessage());
+        return Result.error("系统错误，请联系管理员");
     }
 
     @ExceptionHandler(InvalidKeyException.class)
     public Result invalidKeyExceptionHandler(InvalidKeyException e) {
         log.error("无效密钥异常: " + e.getMessage(), e);
-        return Result.error(e.getMessage());
+        return Result.error("系统错误，请联系管理员");
     }
-
 
 }
 
