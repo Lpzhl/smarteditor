ba3c3bde008b90717a50e770222192a72a7d83a7
lzh <2212384795@qq.com>
2024-08-10 23:53:44 +0800

    feat: 样式id

--- common/src/main/java/hope/smarteditor/common/model/dto/FontSettingDTO.java
+++ common/src/main/java/hope/smarteditor/common/model/dto/FontSettingDTO.java
@@ -1,32 +1,14 @@
 package hope.smarteditor.common.model.dto;
 
-public class FontSettingDTO {
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class FontSettingDTO implements Serializable {
+    private int styleId;       // 样式ID
     private String fontFamily;  // 字体类型
     private int fontSize;       // 字体大小
     private float lineHeight;   // 行间距
 
-    // Getters and Setters
-    public String getFontFamily() {
-        return fontFamily;
-    }
-
-    public void setFontFamily(String fontFamily) {
-        this.fontFamily = fontFamily;
-    }
-
-    public int getFontSize() {
-        return fontSize;
-    }
-
-    public void setFontSize(int fontSize) {
-        this.fontSize = fontSize;
-    }
-
-    public float getLineHeight() {
-        return lineHeight;
-    }
-
-    public void setLineHeight(float lineHeight) {
-        this.lineHeight = lineHeight;
-    }
 }
