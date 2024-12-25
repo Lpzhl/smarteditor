0b5c408ef5c8cc0162b63e38e824c7e0a89472ad
ouoioio <3136695198@qq.com>
2024-12-25 15:11:50 +0800

    Update AiApplicationTests.java
    feat():修改了权限管理模块的用户权限管理

--- ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
+++ ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+  
 class AiApplicationTests {
 lsy ihihi
     @Test
     void contextLoads() {
+    和艰苦好看好看
 后即可兑换法国人gjk、的身份订房ltt萨达阿斯达
     }
 睡大阿松大fsfsfsfsfsf
