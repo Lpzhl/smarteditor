608dcd7a39342ea7e8a51535d2f5fd514ff08b56
ouoioio <3136695198@qq.com>
2024-12-26 15:15:17 +0800

    Update AiApplicationTests.java
    feat():修改了文件上传接口

--- ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
+++ ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
@@ -9,7 +9,7 @@ class AiApplicationTests {
 lsy ihihi
     @Test
     void contextLoads() {
-    和艰苦好看好看
+    和艰苦好看好看jhjhkjhk
 后即可兑换法国人gjk、的身份订房ltt萨达阿斯达lttlttlttlttltt
     }啊阿萨法发hjhjhkjhk
 睡大阿松大fsfsfsfsfsf
