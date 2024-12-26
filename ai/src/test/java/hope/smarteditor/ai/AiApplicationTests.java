a9e1c2264a1a7310eaba74fb425b0475a05b129a
李甜甜 <3181723497@qq.com>
2024-12-26 22:46:34 +0800

    Update AiApplicationTests.java
    feat(): 完成了邀请协同功能中的后端处理邀请数据存储、状态更新，与文档模块开发人员沟通确定协同编辑对接接口，初步实现邀请协同的基本流程

--- ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
+++ ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
@@ -10,7 +10,7 @@ lsy ihihi
     @Test
     void contextLoads() {
     和艰苦好看好看jhjhkjhk你那颗纽扣
-后即可兑换法国人gjk、的身份订房ltt萨达阿斯达lttlttlttlttltt
+后即可兑换法国人gjk、的身份订房ltt萨达阿斯达lttlttlttlttlttltt
     }啊阿萨法发hjhjhkjhk
 睡大阿松大fsfsfsfsfsf
 }
