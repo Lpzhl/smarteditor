a5043cb5016635a528eb2f4ddfe1cf66dc5ea6b5
ouoioio <3136695198@qq.com>
2024-12-26 20:57:58 +0800

    Update AiApplicationTests.java
    fix():修改了素材管理模块的用户管理权限功能

--- ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
+++ ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
@@ -9,7 +9,7 @@ class AiApplicationTests {
 lsy ihihi
     @Test
     void contextLoads() {
-    和艰苦好看好看jhjhkjhk
+    和艰苦好看好看jhjhkjhk你那颗纽扣
 后即可兑换法国人gjk、的身份订房ltt萨达阿斯达lttlttlttlttltt
     }啊阿萨法发hjhjhkjhk
 睡大阿松大fsfsfsfsfsf
