644ec3dee1aa44784ccd6ac1457193ee41a92b41
李甜甜 <3181723497@qq.com>
2024-12-27 10:19:44 +0800

    Update AiApplicationTests.java
    feat(): 完成了前端实现素材上传组件，支持多格式选择

--- ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
+++ ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
@@ -11,7 +11,7 @@ lsy ihihi
   撒旦萨芬
     void contextLoads() {
     和艰苦好看好看jhjhkjhk你那颗纽扣
-后即可兑换法国人gjk、的身份订房ltt萨达阿斯达lttlttlttlttlttlttltt
+后即可兑换法国人gjk、的身份订房ltt萨达阿斯达
     }啊阿萨法发hjhjhkjhkl
 睡大阿松大fsfsfsfsfsf
 }
