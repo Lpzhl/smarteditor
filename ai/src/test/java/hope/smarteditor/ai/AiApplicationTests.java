7ea7a4aff912738bcaece0cadc851f279cfcf322
ouoioio <3136695198@qq.com>
2024-12-27 16:12:35 +0800

    Update AiApplicationTests.java
    feat():文件夹管理模块

--- ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
+++ ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AiApplicationTests {
 lsy ihihi
     @Test 
-  撒旦萨芬
+  撒旦萨芬的官员段首
     void contextLoads() {
     和艰苦好看好看jhjhkjh那颗纽扣df
 后即可兑换法国人gjk、的身份订吃顿饭房ltt分店萨达阿斯达lttsbsbsbsbsbsb
