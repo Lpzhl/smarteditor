4bd7a71c93abca49a8ef13d6968e0b84beca1dbc
lu-yuan-er-xing-zhi <143745311+lu-yuan-er-xing-zhi@users.noreply.github.com>
2024-12-27 11:21:28 +0800

    Update AiApplicationTests.java
     feat(): 完成了订单管理功能中的后端数据存储、状态更新，与接口

--- ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
+++ ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
@@ -10,7 +10,7 @@ lsy ihihi
     @Test 
   撒旦萨芬
     void contextLoads() {
-    和艰苦好看好看jhjhkjhk你那颗纽扣
+    和艰苦好看好看jhjhkjh那颗纽扣
 后即可兑换法国人gjk、的身份订房ltt萨达阿斯达lttsbsbsbsbsbsbs
     }啊阿萨法发hjhjhkjhkl
 睡大阿松大fsfsfsfsfsf
