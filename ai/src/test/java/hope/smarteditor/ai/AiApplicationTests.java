692061b649cdae3d66e4d1f84a09d21c238bc526
ouoioio <3136695198@qq.com>
2024-12-27 20:10:14 +0800

    Update AiApplicationTests.java
    feat():修改了会员中心界面布局

--- ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
+++ ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
@@ -11,7 +11,7 @@ lsy ihihi
   撒旦萨芬的官员段首hkkhk
     void contextLoads() {
     和艰苦好看好看jhjhkjh那颗纽扣df
-后即可兑换法国人gjk、的身份订吃顿饭房ltt分店萨达阿斯达lttsbsb
+后即可兑换法国人gjk、的身份订吃顿饭房ltt分店萨达阿斯达lttsbsb擦拭测试
     }啊阿萨法发hjhjhk二分jhklAAA
 睡大阿松大fsfsfsfsfsf
   ruhene鹿小葵，加油！DDDdoyoudddd
