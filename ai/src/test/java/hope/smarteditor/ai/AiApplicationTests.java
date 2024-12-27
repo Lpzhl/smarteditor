238660a1824e2c6b5e61261e4523dd43d2efbb75
李甜甜 <3181723497@qq.com>
2024-12-27 15:12:02 +0800

    Update AiApplicationTests.java
    feat(): 完成了后端开发上传接口，对接文件存储服务

--- ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
+++ ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
@@ -11,7 +11,7 @@ lsy ihihi
   撒旦萨芬
     void contextLoads() {
     和艰苦好看好看jhjhkjh那颗纽扣df
-后即可兑换法国人gjk、的身份订吃顿饭房ltt分店萨达阿斯达lttsbsbsbsbsbsbskjhgd
+后即可兑换法国人gjk、的身份订吃顿饭房ltt分店萨达阿斯达lttsbsbsbsbsbsb
     }啊阿萨法发hjhjhk二分jhkl
 睡大阿松大fsfsfsfsfsf
 }
