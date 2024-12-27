23dd14fb9e86af3f681bc9cdf9d385d203162cea
ouoioio <3136695198@qq.com>
2024-12-27 08:31:55 +0800

    Update AiApplicationTests.java
    feat():修改了协同编辑部分功能

--- ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
+++ ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
   
 class AiApplicationTests {
 lsy ihihi
-    @Test
+    @Test 
+  撒旦萨芬
     void contextLoads() {
     和艰苦好看好看jhjhkjhk你那颗纽扣
 后即可兑换法国人gjk、的身份订房ltt萨达阿斯达lttlttlttlttlttlttltt
