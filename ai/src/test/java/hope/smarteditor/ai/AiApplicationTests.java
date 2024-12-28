0dd1848d0a1f0dea7807c169f9adea13b844baaa
lu-yuan-er-xing-zhi <143745311+lu-yuan-er-xing-zhi@users.noreply.github.com>
2024-12-28 08:18:25 +0800

    Update AiApplicationTests.java
    feat():删除了市场人员

--- ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
+++ ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
@@ -12,7 +12,7 @@ lsy ihihi
     void contextLoads() {
     和艰苦好看好看jhjhkjh那颗纽扣df
 后即可兑换法国人gjk、的身份订吃顿饭房ltt分店萨达阿斯达lttsbsb擦拭
-    }啊阿萨法发hjhjhk二分jhklAAA
+    }啊阿萨法发hjhjhk二分jhklAAA自行车
 睡大阿松大fsfsfsfsfsf滑旱冰
   ruhene鹿小葵，加油！DDDdoyoudddd军火库
 }
