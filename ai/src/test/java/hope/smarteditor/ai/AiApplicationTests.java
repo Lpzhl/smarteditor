3716a8a2676292720d3791ba8c71534903253572
lu-yuan-er-xing-zhi <143745311+lu-yuan-er-xing-zhi@users.noreply.github.com>
2024-12-28 08:38:55 +0800

    Update AiApplicationTests.java
    feat():删除订单评论

--- ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
+++ ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
@@ -11,7 +11,7 @@ lsy ihihi
   撒旦萨芬的官员段首hkkhk
     void contextLoads() {
     和艰苦好看好看jhjhkjh那颗纽扣df空间和广泛的
-后即可兑换法国人gjk、的身份订吃顿饭房ltt分店萨达阿斯达lttsbsbsb擦拭沙哈
+后即可兑换法国人gjk、的身份订吃顿饭房l空间和规范tt分店萨达阿斯达lttsbsbsb擦拭沙哈
     }啊阿萨法发hjhjhk二分jhklAAA自行车
 睡大阿松大fsfsfsfsfsf滑旱冰阿吉超星咯dsgdx
   ruhene鹿小葵，加油！DDDdoyoudddd军火库时时彩赞
