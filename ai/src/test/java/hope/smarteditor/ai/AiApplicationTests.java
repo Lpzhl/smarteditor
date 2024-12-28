46d43061844ad982aefb88698ad797212ef0ab0b
ouoioio <3136695198@qq.com>
2024-12-28 08:30:13 +0800

    Update AiApplicationTests.java
    feat():修改了回收站界面设计

--- ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
+++ ai/src/test/java/hope/smarteditor/ai/AiApplicationTests.java
@@ -11,7 +11,7 @@ lsy ihihi
   撒旦萨芬的官员段首hkkhk
     void contextLoads() {
     和艰苦好看好看jhjhkjh那颗纽扣df
-后即可兑换法国人gjk、的身份订吃顿饭房ltt分店萨达阿斯达lttsbsbsb擦拭
+后即可兑换法国人gjk、的身份订吃顿饭房ltt分店萨达阿斯达lttsbsbsb擦拭沙哈
     }啊阿萨法发hjhjhk二分jhklAAA自行车
 睡大阿松大fsfsfsfsfsf滑旱冰阿吉超星咯dsgdx
   ruhene鹿小葵，加油！DDDdoyoudddd军火库时时彩赞
